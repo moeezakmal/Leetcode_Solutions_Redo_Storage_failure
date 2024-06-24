b392810a1e4a7953372476bfe21adba88ca5f18b
Moeez Akmal <moeez.akmal@tamu.edu>
2024-06-24 00:13:59 -0500

    Maximum Depth of Binary Tree

---
+++ Max_Depth_Bin_Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    
+    // Time: O(n)
+    // Space: O(1)
+    
+    int maxDepth(TreeNode* root) {
+        if (root==NULL)
+            return 0;
+        
+        return max(1 + maxDepth(root->left), 1 + maxDepth(root->right));
+    }
+};
