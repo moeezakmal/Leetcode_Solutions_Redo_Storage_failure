89f133b5d66f6fac67f39c133fdba7af7f27704b
Moeez Akmal <moeez.akmal@tamu.edu>
2024-07-10 00:33:17 -0500

    Binary Tree Level Order Traversal

---
+++ Binary_Tree_LOT.cpp
@@ -0,0 +1,37 @@
+// Time: O(n)  -> visits each node once
+// Memory: O(n) -> biggest level possible n/2
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+
+        if(root == NULL) 
+            return res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int qLen = q.size();
+            vector<int> level;
+
+            for(int i=0; i<qLen; i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node)
+                {
+                    level.push_back(node->val);
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+            }
+            if (!level.empty())
+                res.push_back(level);
+        }
+        return res;
+    }
+};
