4a37b4fc351a6c8637425bf50b7ef8c6257f6495
Moeez Akmal <moeez.akmal@tamu.edu>
2024-08-18 21:26:34 -0500

    Valid Palindrome (Updated)

--- Valid_Palindrome.c
+++ Valid_Palindrome.c
@@ -1,4 +1,61 @@
+// bool isalphanum(char x);
+
+
+// bool isPalindrome(char* s) {
+//     int l=0;
+//     int r=strlen(s)-1;
+
+//     // printf("%d", s[r]);
+
+//     while (l<r) // middle character is always equal to itself (no need to check)
+//     {
+//         if (!isalphanum(s[l]))
+//         {   
+//             l++;
+//             continue;
+//         }
+
+//         if (!isalphanum(s[r]))
+//         {    
+//             r--;
+//             continue;
+//         }
+        
+//         if (isalphanum(s[l]) && isalphanum(s[r]) && tolower(s[l])==tolower(s[r]))
+//         {
+//             l++;
+//             r--;
+//             continue;
+//         }
+    
+//         return 0;
+//     }
+
+//     return 1;
+// }
+
+
+// bool isalphanum(char x) {
+//     unsigned char uchar = (unsigned char) x;
+
+//     if ('a' <= uchar && uchar <='z' || \
+//         'A' <= uchar && uchar <='Z' || \
+//         '0' <= uchar && uchar <='9' )
+//         return 1;
+//     return 0;
+// }
+
+
+
+
+// Refined version of the above code
+
+// Time: O(n)
+// Space: O(1)
+
+
 bool isalphanum(char x);
+char tolowercase(char x);
 
 
 bool isPalindrome(char* s) {
@@ -12,26 +69,36 @@ bool isPalindrome(char* s) {
         if (!isalphanum(s[l]))
         {   
             l++;
-            continue;
+            // continue;
         }
 
         if (!isalphanum(s[r]))
         {    
             r--;
-            continue;
+            // continue;
         }
         
-        if (isalphanum(s[l]) && isalphanum(s[r]) && tolower(s[l])==tolower(s[r]))
+        if (isalphanum(s[l]) && isalphanum(s[r]) && tolowercase(s[l])==tolowercase(s[r]))
         {
             l++;
             r--;
-            continue;
+            // continue;
         }
-    
-        return 0;
+
+        if (isalphanum(s[l]) && isalphanum(s[r]) && tolowercase(s[l])!=tolowercase(s[r]))
+            return 0;
     }
 
     return 1;
+}
+
+
+char tolowercase(char x) {
+    unsigned char uchar = (unsigned char) x;
+    
+    if ('A' <= uchar && uchar <='Z')
+        return uchar - ('A' - 'a');
+    return uchar;
 }
 
 
